a67769eb5b830734d0cb05f93f5b1e5f82f33f42
Phmik <phm@live.no>
2022-09-27 12:39:00 +0200

    index

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,4 +1,35 @@
-
-
 const API_URL = "https://nf-api.onrender.com";
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkRpbmFPIiwiaWF0IjoxNjY0MjAxNzc3fQ.VT9sYZ1ZSMaKJnOxfnITzLcsAkzTJGf0sWFSp6QvCSw
+
+const options = {
+    headers: {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkRpbmFPIiwiaWF0IjoxNjY0MjAxNzc3fQ.VT9sYZ1ZSMaKJnOxfnITzLcsAkzTJGf0sWFSp6QvCSw'
+    },
+}
+
+const response = await fetch(`${API_URL}/api/v1/social/profiles/name?_posts=true&_following=true&_followers=true`, options)
+const result = await response.json();
+
+
+const userName = document.querySelector(".user-name");
+const posts = document.querySelector(".posts");
+
+
+
+function getUser() {
+    for(let i = 0; i < result.length; i++) {
+        userName.innerHTML = result[i].name;
+    }
+    
+}
+
+getUser();
+
+
+
+
+        
+        
+    
 
