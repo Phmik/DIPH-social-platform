0352a0ec9ff792b63e204f8c9d9b2db40810e95f
DinaOluf <91533917+DinaOluf@users.noreply.github.com>
2022-10-03 16:49:26 +0200

    fetched user information for profile page ++
    and displayed name, made image element if banner exist.

--- src/js/modules/postLogIn.mjs
+++ src/js/modules/postLogIn.mjs
@@ -24,13 +24,15 @@
             formFeedback.innerHTML = json.message;
             formFeedback.style.border = "solid 1px #FF6F6C";
         } else {
-                window.location.href = "index.html";
+                // window.location.href = "index.html";
         }
 
 //Save access token to localStorage
         const accessToken = json.accessToken;
-        if(accessToken) {
+        const name = json.name;
+        if(accessToken && name) {
             localStorage.setItem("accessToken", accessToken);
+            localStorage.setItem("name", name);
         }
     } catch (error) {
         console.log(error);
