eff950eaeecd0ef35f8e2811ecb702c34ab79c2d
DinaOluf <91533917+DinaOluf@users.noreply.github.com>
2022-10-06 17:48:24 +0200

    Coded posts and followers to profile page, started coding follow/unfollow function

--- src/js/modules/deleteWithToken.mjs
+++ src/js/modules/deleteWithToken.mjs
@@ -1,19 +1,19 @@
 /**
- * Posts to API endpoint with accessToken
+ * Deletes entry 
  * @param {string} token the accessToken
  * @param {string} url the url to the API endpoint
- * @param {string} postInfo an object with the information you want to post
+ * @param {string} postInfo an object with the information you want to delete
  * @example
  * ```js
- * // Use this function to post to an API endpoint with an
+ * // Use this function to put/update an entry in the API with
  * // accessToken, a url and the information you want to post.
- * postWithToken(accessToken, API_ENDPOINT_URL, someData);
+ * putWithToken(accessToken, API_ENDPOINT_URL, someData);
  * ```
  */
-async function postWithToken(token, url, postInfo) {
+ async function putWithToken(token, url, postInfo) {
     try {
         const postData = {
-            method: "POST",
+            method: "DELETE",
             headers: {
                 "Content-Type": "application/json",
                 Authorization: `Bearer ${token}`
@@ -29,4 +29,6 @@ async function postWithToken(token, url, postInfo) {
     }
 }
 
-export { postWithToken }; 
+export { putWithToken }; 
+
+//UNDER CONSTRUCTION... --- idk if it works
