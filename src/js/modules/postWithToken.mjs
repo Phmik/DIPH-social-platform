740792b56061b56ac514054f62d7aff453e42c02
Fermain <Fermain@users.noreply.github.com>
2022-10-12 06:23:56 +0200

    Improves error handling

--- src/js/modules/postWithToken.mjs
+++ src/js/modules/postWithToken.mjs
@@ -2,7 +2,7 @@
  * Posts to API endpoint with accessToken
  * @param {string} token the accessToken
  * @param {string} url the url to the API endpoint
- * @param {string} postInfo an object with the information you want to post
+ * @param {object} postInfo an object with the information you want to post
  * @example
  * ```js
  * // Use this function to post to an API endpoint with an
@@ -21,11 +21,15 @@ async function postWithToken(token, url, postInfo) {
             body: JSON.stringify(postInfo),
         };
         const response = await fetch(url, postData);
-        const json = await response.json();
 
-        window.location.reload();
+        if (response.ok) {
+            window.location.reload();
+        } else {
+            throw new Error("Something went wrong");
+        }
     } catch (error) {
         console.log(error);
+        alert(error);
     }
 }
 
